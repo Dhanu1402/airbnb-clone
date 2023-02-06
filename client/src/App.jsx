87870570e7c66e0d0170b9d50550de60955aaa6c
Dhanu1402 <dhanukumar2000@gmail.com>
2023-02-06 12:00:15 +0530

    Handling Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './components/Index';
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Index />} />
-    </Routes>
+    <BrowserRouter>
+      <main>
+        <container className="mt-3">
+          <Routes>
+            <Route index element={<Index />} />
+          </Routes>
+        </container>
+      </main>
+    </BrowserRouter>
   );
 }
 
