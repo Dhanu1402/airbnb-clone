ecf69c2363c090eef82bfb9f918aeb60df2362f7
Dhanu1402 <dhanukumar2000@gmail.com>
2023-02-19 11:33:45 +0530

    Small bugfixes and code refactoring

--- client/src/components/AccountNav.jsx
+++ client/src/components/AccountNav.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function AccountNav() {
+  const { pathname } = useLocation();
+  let subpage = pathname.split('/')?.[2];
+  if (subpage === undefined) {
+    subpage = 'profile';
+  }
+
   function linkClasses(type = null) {
     let classes = 'inline-flex gap-1 py-2 px-6 rounded-full';
-    if (type === false) {
+    if (type === subpage) {
       classes += ' bg-primary text-white'; // put space after 1st comma
     } else {
       classes += ' bg-gray-200';
